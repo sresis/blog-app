fd447694173ce74c899295b96d4c6cbea9ccfc98
Stephanie Resis <sresis@umich.edu>
2021-01-07 19:57:30 -0800

    initial commit

---
+++ src/server/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { sum } from "../core/math";
+
+
+const buildDir = path.join(process.cwd() + "/build");
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(buildDir));
+app.get("/ping", function (req, res) {
+    return res.json(`${sum(10, 4)}`);
+  });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(buildDir, "index.html"));
+});
+
+
+
+const port = 3001;
+console.log("checking port", port);
+app.listen(port, () => {
+  console.log(`Server now listening on port: ${port}`);
+});
